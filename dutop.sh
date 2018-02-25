790da08467982361519f2bab721f5548aae2dbec
woocashb <woocashboczkaja@gmail.com>
2018-02-25 18:37:56 +0100

    added total size for result set in dutop.sh

--- dutop.sh
+++ dutop.sh
@@ -5,4 +5,12 @@
 
 DIR=${1%/}
 HEAD_CNT=${2:-10}
-du -s --block-size=MB $DIR/* | sort -rn -k1 | head -n $HEAD_CNT
+UNIT=${3:-M}
+
+top=$(du -s --block-size=${UNIT} $DIR/* | sort -rn -k1 | head -n $HEAD_CNT)
+
+printf '%s' "$top"
+
+sum=$(echo "$top" | awk -F M '{print $1}' | awk '{ sum += $1 } END { print sum }')
+
+printf '\n\nRazem: %s%s\n' "$sum" "$UNIT"

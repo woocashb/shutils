0f054708b734515e7c704692a3bb9c5b51c7343b
woocash <woocashboczkaja@gmail.com>
2018-04-01 21:01:58 +0200

    sshcfg_mgr: dodano funkcje listujaca i usuwajaca wpisy w ssh config

---
+++ sshcfg_mgr.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/bash -x
+
+tag=$2
+ip=$3
+usr=$4
+proxy=$5
+
+SSH_CFG=~/.ssh/configtest
+
+USAGE_FAIL=1
+TAG_DUPLICATE=2
+
+
+ENTRY_TEMPLATE=$(printf "
+Host $tag
+  HostName $ip
+  User $usr
+"
+)
+
+add(){
+
+if [[ ! -z $proxy ]];then
+  ENTRY_PROXY="ProxyCommand ssh -q -W %h:%p $proxy"
+  ENTRY_TEMPLATE=$(printf "%s\n  %s\n" "$ENTRY_TEMPLATE" "$ENTRY_PROXY")
+fi
+
+printf "\n%s" "$ENTRY_TEMPLATE" >> $SSH_CFG
+
+exit 0;
+}
+
+chk_add_arg(){
+add_argc=$1
+if [[ $add_argc -lt 3 || $add_argc -gt 4 ]];then
+  usage
+fi
+if [[ $(grep -c -i $tag $SSH_CFG) -ne 0 ]];then
+  echo "Etykieta \"$tag\" juz istnieje."
+  exit $TAG_DUPLICATE
+fi
+return 0
+}
+
+remove(){
+cp -p $SSH_CFG ${SSH_CFG}.backup
+sed -i "/${tag}/,+3d" $SSH_CFG
+
+exit 0
+}
+
+list(){
+grep -A 4 -i $tag $SSH_CFG
+exit 0
+}
+
+usage(){
+printf "
+Dodaj/usun wpisy dla hostow w ~/.ssh/config
+sshcfg [-a] host_tag ip_address user_name [proxy_host]
+       [-r] host_tag
+"
+exit $USAGE_FAIL;
+}
+
+while getopts "arl:" action;do
+     case $action in
+        a) add $tag $ip $usr $proxy;;
+        r) remove $tag ;;
+        l) list $tag ;;
+        \?) usage ;;
+     esac
+done
+
+usage
+

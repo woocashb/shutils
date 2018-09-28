06fde0b7c49b63d8b385e60d722560547bb4ee28
woocash <woocashboczkaja@gmail.com>
2018-09-28 22:23:31 +0200

    dutop.sh: drobne poprawki, nic ciekawego

--- dutop.sh
+++ dutop.sh
@@ -11,6 +11,6 @@ top=$(du -s --block-size=${UNIT} $DIR/* | sort -rn -k1 | head -n $HEAD_CNT)
 
 printf '%s' "$top"
 
-sum=$(echo "$top" | awk -F M '{print $1}' | awk '{ sum += $1 } END { print sum }')
+sum=$(echo "$top" | awk -F ${UNIT} '{print $1}' | awk '{ sum += $1 } END { print sum }')
 
 printf '\n\nRazem: %s%s\n' "$sum" "$UNIT"
